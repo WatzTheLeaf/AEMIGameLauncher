e8e8b1e0484b60fd3cc2008b63435eddf2610d7f
Watz <watztheengineer@gmail.com>
2025-04-01 14:41:35 -0300

    add game preview

--- src/components/GameBar.vue
+++ src/components/GameBar.vue
@@ -7,19 +7,24 @@ const props = defineProps({
     activeGameId: Number
 });
 
+const nullGame = { id: 0, name: "", image: "/src/assets/blank.jpg", description: "", authors: [""]};
+
 const emit = defineEmits(["update:activeGameId"]);
 
-const activeGame = computed(() => props.games.find(game => game.id === props.activeGameId));
+const activeGameIndex = computed(() => props.games.findIndex(game => game.id === props.activeGameId));
+const activeGame = computed(() => props.games[activeGameIndex.value] || null);
 
-const canNavigateUp = computed(() => props.games.findIndex(game => game.id === props.activeGameId) > 0);
-const canNavigateDown = computed(() => props.games.findIndex(game => game.id === props.activeGameId) < props.games.length - 1);
+const canNavigateUp = computed(() => activeGameIndex.value > 0);
+const canNavigateDown = computed(() => activeGameIndex.value < props.games.length - 1);
+
+const previousGame = computed(() => canNavigateUp.value ? props.games[activeGameIndex.value - 1] : null);
+const nextGame = computed(() => canNavigateDown.value ? props.games[activeGameIndex.value + 1] : null);
 
 const navigateGames = (direction) => {
-    const currentIndex = props.games.findIndex(game => game.id === props.activeGameId);
-    if (direction === "up" && canNavigateUp.value) {
-        emit("update:activeGameId", props.games[currentIndex - 1].id);
-    } else if (direction === "down" && canNavigateDown.value) {
-        emit("update:activeGameId", props.games[currentIndex + 1].id);
+    if (direction === "up" && previousGame.value) {
+        emit("update:activeGameId", previousGame.value.id);
+    } else if (direction === "down" && nextGame.value) {
+        emit("update:activeGameId", nextGame.value.id);
     }
 };
 
@@ -33,7 +38,11 @@ onUnmounted(() => window.removeEventListener("keydown", handleKeyDown));
 </script>
 
 <template>
-    <div class="h-full p-4 w-2/5 overflow-y-auto text-center flex flex-col justify-center items-center">
+    <div class="h-full p-4 w-2/5 text-center overflow-y-hidden flex flex-col justify-center items-center">
+
+        <Game v-if="previousGame" :game="previousGame" class="w-1/3 opacity-50"/>
+        <Game v-else="previousGame" :game="nullGame" class="w-1/3 opacity-0"/>
+
         <div v-if="canNavigateUp" class="text-2xl cursor-pointer mb-2 text-slate-50" @click="navigateGames('up')">▲</div>
         <div v-else class="text-2xl cursor-pointer mb-2 text-slate-50">‎</div>
 
@@ -41,5 +50,9 @@ onUnmounted(() => window.removeEventListener("keydown", handleKeyDown));
 
         <div v-if="canNavigateDown" class="text-2xl cursor-pointer mt-2 text-slate-50" @click="navigateGames('down')">▼</div>
         <div v-else class="text-2xl cursor-pointer mt-2 text-slate-50">‎</div>
+
+        <Game v-if="nextGame" :game="nextGame" class="w-1/3 opacity-50"/>
+        <Game v-else="nextGame" :game="nullGame" class="w-1/3 opacity-0"/>
+
     </div>
 </template>
