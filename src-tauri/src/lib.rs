47c8da52e9b3ef0a550275c65f0d3fd1417072f5
Watz <watztheengineer@gmail.com>
2025-04-01 18:19:06 -0300

    partial creation of config

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs::{create_dir, exists};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,9 +7,22 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    init();
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+pub fn init(){
+    match create_dir("./AEMIGameLauncherConfig") {
+        Ok(_) => {println!("Config directory was successfully created");}
+        Err(_) => {}
+    }
+    match exists("./AEMIGameLauncherConfig") {
+        Ok(_) => {}
+        Err(_) => {panic!("Config folder do not exist or is unreadable");}
+    }
 }
